c8d89048fbf62dd173f362488343f65dc0f4ab77
jholkeboer <jholkeboer@users.noreply.github.com>
2015-06-04 18:43:43 -0700

    landing page layout

--- login.php
+++ login.php
@@ -3,8 +3,31 @@
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-	<h1>Login Page</h1>
+<div class="titlebar">
+	<a href="landing.html">auxjockey<a>
+</div>
+
+<div class="sidebar">
+	<ul>
+		<li><a href="login.php">Log In</a></li>
+		<li><a href="signup.php">Sign Up</a></li>
+	</ul>
+</div>
+
+<div class="viewport">
+	<h1>log in to auxjockey:</h1>
 	<form>
-		
+		<div>
+			<label>Username: </label>
+			<input type="text" name="username-input" id="username-input">
+		</div>
+		<div>
+			<label>Password: </label>
+			<input type="text" name="password-input" id="password-input">
+		</div>
+		<div>
+			<input type="button" value="Log In" onclick="loginScript()" id="login-button">
+		</div>
 	</form>
+</div>
 </body>
