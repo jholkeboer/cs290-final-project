815398a91b3e75369383ca44f3d5a3a065bd1ff0
jholkeboer <jholkeboer@users.noreply.github.com>
2015-06-07 09:33:21 -0700

    logout working

--- logout.php
+++ logout.php
@@ -1,8 +1,9 @@
 <?php
 include "infostash.php";
-session_start;
+session_start();
+unset($_SESSION["loginStatus"]); 
 $_SESSION = array();
 session_destroy;
-header("Location: {$redirect}/landing.php", true);
+header("Location: {$redirect}/landing.php");
 die();
 ?>
